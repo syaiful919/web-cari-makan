ea28a10314408c9db6a3fa2ba2dcfbc5c8d09d5e
Syaiful Salam <syaifulsalam@192.168.1.7>
2020-12-02 23:33:43 +0700

    add sections, fix accessibility

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,6 @@ import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "../styles/responsive.css";
 
-console.log("Hello Coders! :)");
-
 const menu = document.querySelector("#menu");
 const hero = document.querySelector(".hero");
 const main = document.querySelector("main");
@@ -12,11 +10,8 @@ const header = document.querySelector("header");
 const logo = document.querySelector(".logo");
 const navItem = document.querySelectorAll(".nav__item a");
 
-let isOpen = false;
-
 menu.addEventListener("click", function (event) {
   drawer.classList.toggle("open");
-  isOpen = true;
   navItem.forEach(function (x) {
     x.style.color = "#2c3e50";
   });
@@ -51,7 +46,9 @@ function scrollFunction() {
     logo.style.color = "white";
     menu.style.color = "white";
     navItem.forEach(function (x) {
-      if (!isOpen) {
+      if (drawer.classList.contains("open") && window.screen.width < 600) {
+        x.style.color = "#2c3e50";
+      } else {
         x.style.color = "white";
       }
     });
